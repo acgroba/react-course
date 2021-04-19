3ec69851d9be58a2fc3a1edc9578db3110d42668
abcg <abcg@gmv.com>
2021-04-19 08:10:37 +0200

    (youtube hooks) Custom hook added

--- youtube hooks/src/components/App.js
+++ youtube hooks/src/components/App.js
@@ -1,30 +1,28 @@
-import React from "react";
-import youtube from "../api/youtube";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
-  onSearchSubmit = async (term) => {
-    const response = await youtube.get("/search", { params: { q: term } });
-    this.setState({ videos: response.data.items });
-  };
+import useVideos from "../hooks/useVideos";
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
-        <VideoDetail video={this.state.selectedVideo}></VideoDetail>
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        ></VideoList>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("cats");
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search}></SearchBar>
+      <VideoDetail video={selectedVideo}></VideoDetail>
+      <VideoList
+        onVideoSelect={(video) => {
+          setSelectedVideo(video);
+        }}
+        videos={videos}
+      ></VideoList>
+    </div>
+  );
+};
 
 export default App;
--- youtube hooks/src/components/SearchBar.js
+++ youtube hooks/src/components/SearchBar.js
@@ -1,29 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.term);
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Search Video</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            ></input>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Search Video</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          ></input>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
